4a6ec0bd6ddc4dcd6ca1fa0a1240bd1c51a2c71e
Adama SAMAKE <as91404togo@gmail.com>
2024-08-11 16:45:58 +0000

    Update Main.py: Final dataframe

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ import numpy as np
 # Initialize the spark session
 spark = (
     SparkSession.builder.appName("Datacamp Pyspark Tutorial")
-    .config("spark.log.level", 'ERROR')
+    .config("spark.log.level", "ERROR")
     .getOrCreate()
 )
 
@@ -37,58 +37,46 @@ df.groupBy("Country").agg(countDistinct("CustomerID").alias("Nb_Customers")).ord
 
 # The last and the first purchase
 spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
-df = df.withColumn("date", to_timestamp(df.InvoiceDate, 'dd/MM/yyyy HH:mm'))
-df.select(min('date').alias('First_Purchase')).show()
-df.select(max('date').alias('Last_Purchase')).show()
+df = df.withColumn("date", to_timestamp(df.InvoiceDate, "dd/MM/yyyy HH:mm"))
+df.select(min("date").alias("First_Purchase")).show()
+df.select(max("date").alias("Last_Purchase")).show()
 
-"""
+# DATA PRE-PROCESSING
+# Computation of recency
+last_purchase_timestamp = (
+    df.select(min("date").alias("First_Purchase"))
+    .collect()[0]
+    .asDict()
+    .get("First_Purchase")
+)
 
-df = df.withColumn("from_date", lit("12/1/10 08:26"))
-df = df.withColumn('from_date',to_timestamp("from_date", 'yy/MM/dd HH:mm'))
+df = df.withColumn("from_date", lit(last_purchase_timestamp))
 
-df2=df.withColumn('from_date',to_timestamp(col('from_date'))).withColumn('recency',col("date").cast("long") - col('from_date').cast("long"))
+df2 = df.withColumn("from_date", to_timestamp(col("from_date"))).withColumn(
+    "recency", col("date").cast("long") - col("from_date").cast("long")
+)
+df2 = df2.join(
+    df2.groupBy("CustomerID").agg(max("recency").alias("recency")),
+    on="recency",
+    how="leftsemi",
+)
+df2.show(5, 0)
 
-df2 = df2.join(df2.groupBy('CustomerID').agg(max('recency').alias('recency')),on='recency',how='leftsemi')
+# Computation of frequency
+df_freq = df2.groupBy("CustomerID").agg(count("InvoiceDate").alias("frequency"))
+df_freq.show(5, 0)
 
-df2.show(5,0)
-df2.printSchema() # To show info of dataframe like info() in pandas
+# Join the frequency data with the last dataframe
+df3 = df2.join(df_freq, on="CustomerID", how="inner")
+df3.show(5, 0)
 
-df_freq = df2.groupBy('CustomerID').agg(count('InvoiceDate').alias('frequency'))
-df_freq.show(5,0)
+# Computation of monetary value
+m_val = df3.withColumn("TotalAmount", col("Quantity") * col("UnitPrice"))
+m_val = m_val.groupBy("CustomerID").agg(sum("TotalAmount").alias("monetary_value"))
 
-df3 = df2.join(df_freq,on='CustomerID',how='inner')
-df3.printSchema()
-
-m_val = df3.withColumn('TotalAmount',col("Quantity") * col("UnitPrice"))
-
-m_val = m_val.groupBy('CustomerID').agg(sum('TotalAmount').alias('monetary_value'))
-
-finaldf = m_val.join(df3,on='CustomerID',how='inner')
-
-finaldf = finaldf.select(['recency','frequency','monetary_value','CustomerID']).distinct()
-
-assemble=VectorAssembler(inputCols=[
-    'recency','frequency','monetary_value'
-], outputCol='features')
-
-assembled_data=assemble.transform(finaldf)
-
-scale=StandardScaler(inputCol='features',outputCol='standardized')
-
-data_scale=scale.fit(assembled_data)
-
-data_scale_output=data_scale.transform(assembled_data)
-
-data_scale_output.select('standardized').show(2,truncate=False)
-
-cost = np.zeros(10)
-
-evaluator = ClusteringEvaluator(predictionCol='prediction', featuresCol='standardized',metricName='silhouette', distanceMeasure='squaredEuclidean')
-
-for i in range(2,10):
-    KMeans_algo=KMeans(featuresCol='standardized', k=i)
-    KMeans_fit=KMeans_algo.fit(data_scale_output)
-    output=KMeans_fit.transform(data_scale_output)
-    cost[i] = KMeans_fit.summary.trainingCost
-
-"""
+# Merge all data
+finaldf = m_val.join(df3, on="CustomerID", how="inner")
+finaldf = finaldf.select(
+    ["recency", "frequency", "monetary_value", "CustomerID"]
+).distinct()
+finaldf.show(5, 0)
