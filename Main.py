8da75d4d327376848491077f48127fce7a828181
Adama SAMAKE <as91404togo@gmail.com>
2024-08-17 14:54:50 +0000

    k-means client segmentation and cluster analysis

--- Main.py
+++ Main.py
@@ -2,10 +2,15 @@
 from pyspark.sql.context import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+
 from pyspark.ml.feature import VectorAssembler, StandardScaler
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.evaluation import ClusteringEvaluator
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # Initialize the spark session
@@ -37,7 +42,7 @@ df.groupBy("Country").agg(countDistinct("CustomerID").alias("Nb_Customers")).ord
 
 # The last and the first purchase
 spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
-df = df.withColumn("date", to_timestamp(df.InvoiceDate, "dd/MM/yyyy HH:mm"))
+df = df.withColumn("date", to_timestamp(df.InvoiceDate, "dd/MM/yy HH:mm"))
 df.select(min("date").alias("First_Purchase")).show()
 df.select(max("date").alias("Last_Purchase")).show()
 
@@ -51,6 +56,7 @@ last_purchase_timestamp = (
 )
 
 df = df.withColumn("from_date", lit(last_purchase_timestamp))
+df.select('from_date').distinct().show()
 
 df2 = df.withColumn("from_date", to_timestamp(col("from_date"))).withColumn(
     "recency", col("date").cast("long") - col("from_date").cast("long")
@@ -80,3 +86,64 @@ finaldf = finaldf.select(
     ["recency", "frequency", "monetary_value", "CustomerID"]
 ).distinct()
 finaldf.show(5, 0)
+
+# Vectorize columns into features and Standardize the data
+assembled_data = VectorAssembler(
+    inputCols=["recency", "frequency", "monetary_value"], outputCol="features"
+).transform(finaldf)
+
+data_scale_output = (
+    StandardScaler(inputCol="features", outputCol="standardized")
+    .fit(assembled_data)
+    .transform(assembled_data)
+)
+data_scale_output.select("standardized").show(2, truncate=False)
+
+# Build the machine learning model
+cost = np.zeros(10)
+
+evaluator = ClusteringEvaluator(
+    predictionCol="prediction",
+    featuresCol="standardized",
+    metricName="silhouette",
+    distanceMeasure="squaredEuclidean",
+)
+
+# Using Eblow method to find the best value of k
+for i in range(2, 10):
+    KMeans_algo = KMeans(featuresCol="standardized", k=i)
+    KMeans_fit = KMeans_algo.fit(data_scale_output)
+    output = KMeans_fit.transform(data_scale_output)
+    cost[i] = KMeans_fit.summary.trainingCost
+
+df_cost = pd.DataFrame(cost[2:])
+df_cost.columns = ["cost"]
+
+new_col = range(2, 10)
+df_cost.insert(0, "cluster", new_col)
+
+plt.plot(df_cost.cluster, df_cost.cost)
+plt.xlabel("Number of Clusters")
+plt.ylabel("Score")
+plt.title("Elbow Curve")
+plt.savefig("fig.png")
+
+# Building k-means algorithm
+KMeans_algo = KMeans(featuresCol="standardized", k=4)
+KMeans_fit = KMeans_algo.fit(data_scale_output)
+
+preds = KMeans_fit.transform(data_scale_output)
+preds.show(5)
+
+# Analysis of clusters
+df_viz = preds.select(
+    "recency", "frequency", 
+    "monetary_value", "prediction"
+).toPandas()
+avg_df = df_viz.groupby(["prediction"], as_index=False).mean()
+
+list1 = ["recency", "frequency", "monetary_value"]
+for i in list1:
+    plt.clf()
+    sns.barplot(x="prediction", y=str(i), data=avg_df)
+    plt.savefig(f"fig-{i}.png")
--- README.md
+++ README.md
@@ -26,3 +26,9 @@ By the end of this tutorial, you will be familiar with the following concepts:
 - Building a K-Means clustering model
 - Interpreting the model
 
+## Cluster Analysis Results
+
+- **Cluster 0**: Customers in this segment display high recency and frequency and monetary value. They are customers who are likely to be one of our best customers.
+- **Cluster 1**: Users in this cluster display high recency but haven’t been seen spending much on the platform. They also don’t visit the site often. This indicates that they might be newer customers who have just started doing business with the company.
+- **Cluster 2**: Customers in this segment display medium recency and frequency but don't spend much money on the platform. They are low potential customers.
+- **Cluster 3**: The final segment comprises users who display high recency and are rarely on the platform. However, they spend much money on the platform, which might mean that they tend to select expensive items in each purchase.
